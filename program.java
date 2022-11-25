fc70722f52e52d4a536f2bffc02fc41979b382b9
Olga <beauty-90@list.rugit config --global user.email beauty-90@list.ru>
2022-11-25 21:42:14 +0300

    sem1

---
+++ program.java
@@ -0,0 +1,30 @@
+public class program {
+    static boolean isTriangular(int num) {
+        // Base case
+        if (num < 0)
+            return false;
+
+        // A Triangular number must be
+        // sum of first n natural numbers
+        int sum = 0;
+
+        for (int n = 1; sum <= num; n++) {
+            sum = sum + n;
+            if (sum == num)
+                return true;
+        }
+
+        return false;
+    }
+
+    // Driver code
+    public static void main(String[] args) {
+        int n = 55;
+        if (isTriangular(n))
+            System.out.print("The number "
+                    + "is a triangular number");
+        else
+            System.out.print("The number"
+                    + " is NOT a triangular number");
+    }
+}
